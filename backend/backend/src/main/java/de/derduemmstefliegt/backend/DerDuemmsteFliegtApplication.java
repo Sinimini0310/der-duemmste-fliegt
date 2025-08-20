46e8273ca002a07cdf3a22cc36355400be78218e
Sinan Bilen <Sinan.Bilen@vkb.de>
2025-08-20 17:10:01 +0200

    vorlage fertig

---
+++ backend/backend/src/main/java/de/derduemmstefliegt/backend/DerDuemmsteFliegtApplication.java
@@ -0,0 +1,13 @@
+package de.derduemmstefliegt.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DerDuemmsteFliegtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DerDuemmsteFliegtApplication.class, args);
+	}
+
+}
---
+++ backend/backend/src/test/java/de/derduemmstefliegt/backend/DerDuemmsteFliegtApplicationTests.java
@@ -0,0 +1,13 @@
+package de.derduemmstefliegt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DerDuemmsteFliegtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
